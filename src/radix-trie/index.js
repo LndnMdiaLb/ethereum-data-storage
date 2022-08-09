1ac2c582d5a2cc49bf9611993dc7be36ae8441f0
Andreas Dilaveris <projects@thelondonmedialab.com>
2022-08-09 09:18:20 +0300

    Radix Trie with Mock Database

--- src/radix-trie/index.js
+++ src/radix-trie/index.js
@@ -1,68 +1,26 @@
-/*********************
- 
-    Minimal Radix Trie
-
-*********************/
-
-
+const fs = require("fs");
+const path = require("path");
 
 class TrieNode {
 
     constructor(isTerminator){
-        /* 
-            the first person to figure out why I chose the value 16 as the terminator value
-            will win a prize
-        */
         if(isTerminator) this['16'] = 1;
     }
 
-    /* 
-        accepts a path: a string of characters
-        while moving through the past string character by character
-        recursively build the trie structure 
-    */
-
     put(path){
-        /* we use the first key (character) of the path */
         const path_key = path[0];
-        /* remove the key from rest of path */
         const path_rest = path.substring(1);
-
-        /* 
-            if the key does not exist on this TrieNode instance
-        */
         if(!this[path_key]){
-            /* 
-                instantiate a TrieNode and store in the key prop of this TrieNode
-                if no path left set isTerminator 
-            */
             this[path_key] = new TrieNode(path_rest.length==0);
-            /* 
-                if path characters still exist in path string 
-                pass to .put in TrieNode instance stored at key property just created
-                this is recursive
-            */
             if(path_rest.length) this[path_key].put(path_rest);
-
-        /* if the key already exists on the TrieNode instance */
         }else{
-            /* 
-                if the path has been completed set terminator
-                if it isn't recursively .put path 
-            */
             path_rest.length ? this[path_key].put(path_rest) : this[path_key]['16'] = 1;
         }
 
     }
-
-    /*
-        recursively look for path till complition whithin trie structure 
-    */
    
     findPath(path){
-        /* we use the first key (character) of the path */
         const path_key = path[0];
-        /* remove the key from rest of path */
         const path_rest = path.substring(1);
         if(path.length==1){
             return this[path_key]['16'] && this[path_key];
@@ -83,26 +41,22 @@ const trie = new TrieNode;
 trie.put('word');
 trie.put('wood');
 
-
-/*
-    tests trie
+/* 
+    create our Radix Trie Databse
 */
 
-
-function existsInTrie(word) {
-    if (!!trie.findPath(word)){
-        console.log(`found "${word}" in radix-trie`);
-    } else{
-        console.log(`did not find "${word}" in radix-trie`);
+function saveToDB(){
+    const db = path.resolve(__dirname, './db');
+    if (!fs.existsSync(db)){
+        fs.mkdirSync(db);
     }
+    fs.writeFileSync(path.resolve(db, 'trie.json'), JSON.stringify(trie));
 }
 
+console.log('\n');
+
+console.log(trie);
 
 console.log('\n');
 
-existsInTrie('word');
-existsInTrie('wood');
-existsInTrie('woo');
-existsInTrie('hi');
-
-console.log('\n');
+saveToDB();
