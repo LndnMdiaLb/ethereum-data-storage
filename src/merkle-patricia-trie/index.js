0d3d04b8a54c30ddb11f819bb14b4c3185d8e832
Andreas Dilaveris <projects@thelondonmedialab.com>
2022-08-09 13:35:06 +0300

    Introduction to Branch Node & history

--- src/merkle-patricia-trie/index.js
+++ src/merkle-patricia-trie/index.js
@@ -1,11 +1,9 @@
 const path = require("path");
 const { Trie } = require("@ethereumjs/trie");
-
 /*
     LevelDb is a key value storage.  Its a look up table think excel sheet with 2 columns
 */
 const {Level} = require("level");
-
 const { keccak256:keccak } = require("ethereum-cryptography/keccak");
 
 const dbPath = path.resolve(__dirname,"db");
@@ -15,13 +13,15 @@ const trie = new Trie({db});
 
 (async ()=>{
 
-    const key = Buffer.from('abcd', 'hex');
-    const value = Buffer.from([1]);
-
-    await trie.put(key, value); 
+    await trie.put(Buffer.from('abcd', 'hex'),  Buffer.from([1]));
+    /*
+        adding a second entry into the trie. Note the first nibble is different from the last entry
+    */ 
+    await trie.put(Buffer.from('bb', 'hex'), Buffer.from([2])); 
 
     const trieNode = await trie.lookupNode(trie.root);
 
+    console.log(trieNode);
 
     console.log(`\nTrie Node in serialize() format :\n`);
 
@@ -29,17 +29,14 @@ const trie = new Trie({db});
 
     console.log(`\nKeccak hash of selialised bytes :\n`);
 
-    console.log(Buffer.from(keccak(trieNode.serialize())))
+    console.log(Buffer.from(keccak(trieNode.serialize())));
 
     console.log('\n\nLevelDB database entries:\n');
 
     const entries = await db.iterator({ limit: 100 }).all()
 
-    /* 
-        the level db database stores the serialised data using the hash of the serialised data as a key 
-    */
     for (const [key, value] of entries) {
-        console.log(`key:\n\n`,key,'\n\nvalue:\n\n',value);
+        console.log(key,' | ',value);
     }
 
     console.log('\n');
