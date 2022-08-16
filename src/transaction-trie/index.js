7d2fca2180e218e091022c1c85ed9f46e1b54632
Andreas Dilaveris <projects@thelondonmedialab.com>
2022-08-16 12:23:54 +0300

    The Transaction Trie key:valy signature

--- src/transaction-trie/index.js
+++ src/transaction-trie/index.js
@@ -1,9 +1,4 @@
-
-
-const { Trie } = require("@ethereumjs/trie");
-/* 
-    2 blocks of 2 complexities
-*/
+const {Trie, LeafNode, ExtensionNode, BranchNode} = require("@ethereumjs/trie");
 const { smallBlock, largeBlock } = require("../constants");
 const {
     AccessListEIP2930Transaction: EIP2930Transaction,
@@ -12,21 +7,15 @@ const {
 } = require("@ethereumjs/tx");
 const rlp = require("rlp");
 const ethers = require("ethers");
+const { keccak256 } = require("ethers/lib/utils");
 
 const trie = new Trie();
-/* 
-    you can use largeBlock to create more complex trie 
-*/
 const blockHash = smallBlock;
 let provider = new ethers.providers.JsonRpcProvider();
 let block;
 
 function changeSyntax (tx){
     tx = {...tx};
-    /*
-        in returned block data transaction has 2 properties 'input' and 'gas'
-        we modify these as they are expected as 'data' and 'gasLimit' in ethereumjs 
-    */
     tx.data = tx.input;
     delete tx.input;
     tx.gasLimit = tx.gas;
@@ -37,43 +26,63 @@ function changeSyntax (tx){
 async function recreateTransactionTrie (block, trie){
     await Promise.all(block.transactions.map((tx, i) => 
             (async (tx, i) => {
-                /* modify transaction syntax */
                 tx = changeSyntax(tx);
                 let TransactionClass;
                 switch (tx.type){
-                    /*
-                        ethereumjs provides individual classes to work with different transaction types
-                    */
                     case '0x0': TransactionClass = LegacyTransaction; break;
                     case '0x1': TransactionClass = EIP2930Transaction; break;
                     case '0x2': TransactionClass = EIP1559Transaction; break;
                 }
                 const serializedTx = TransactionClass.fromTxData(tx).serialize();
-                /*
-                    The canonical rule for recreating a transaction trie is 
+                /* 
+                    something strange ...
+                    the blockchain stores values 0x0 expecting them to be interpreted as 0x80 in rlp! (infura ?)
                     
-                    key : rlp encode the transactionIndex ( position in the transaction array in block )
-                    value : serialized transaction ( type | rlp encoded transaction ) 
+                     ¯\_(ツ)_/¯
+
+                     rlp.encode("0x0")  === Uint8Array(1) [ 0 ]
+                     rlp.encode(0)      === Uint8Array(1) [ 128 ]
                 */
+                console.log('\n\ntransaction:\n');
+                console.log(`key: rlp encoded transaction index: ${tx.transactionIndex}\n`);
+                console.log(Buffer.from(rlp.encode(i)));
+                console.log('\nvalue: rlp encoded transaction \n')
+                console.log(serializedTx);
+                console.log(`\n\nis keccak hash of serialised transaction equal to hash in block: ${tx.hash ==keccak256(serializedTx)}`);
                 return await trie.put( rlp.encode(i), serializedTx);
             })(tx, i) 
         )); 
 };
 
 (async (blockHash)=> {
-    /*
-        why not use :  provider.getBlock( block ) ?
-
-        ethers getBlock method offers lots of convenince functionality (ex. turning values into BigNumbers)
-        but purposefully, internally creates and returns an object with commonly useful block data.  
-        we want all block data. so we use the more generic send rpc method
-    */
     block = await provider.send("eth_getBlockByHash", [ blockHash, true ]);
     await recreateTransactionTrie(block, trie);
-    /* 
-        check the 
-        block.transactionsRoot stored in the block
-        matches our local trie's root hash
-    */
-    console.log(`block.transactionsRoot matches trie.root : ${block.transactionsRoot === ethers.utils.hexlify(trie.root)}`);
+    console.log('\n\nStructure of our Trie\n')
+    await inspectTrie(trie);
+
 })(blockHash);
+
+
+async function inspectTrie(trie){
+    await trie.walkTrie(trie.root, 
+        async (nodeRef, node, keyProgress, walkController) => {
+            
+            walkController.allChildren(node)
+
+            if (node instanceof LeafNode) {
+                console.log("LeafNode");
+                return 
+            }
+
+            if (node instanceof ExtensionNode) {
+                console.log("ExtensionNode");
+                return 
+            }
+            
+            if (node instanceof BranchNode) {
+                console.log("BranchNode");
+                return 
+            } 
+
+        });
+}
