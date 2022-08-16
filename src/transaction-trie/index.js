c25cc753ae3e3fa1784a6020a70159f104246c3b
Andreas Dilaveris <projects@thelondonmedialab.com>
2022-08-16 12:25:04 +0300

    Custom serialization of Transaction Trie puts

--- src/transaction-trie/index.js
+++ src/transaction-trie/index.js
@@ -1,21 +1,18 @@
-const {Trie, LeafNode, ExtensionNode, BranchNode} = require("@ethereumjs/trie");
+const {Trie} = require("@ethereumjs/trie");
 const { smallBlock, largeBlock } = require("../constants");
-const {
-    AccessListEIP2930Transaction: EIP2930Transaction,
-    FeeMarketEIP1559Transaction: EIP1559Transaction,
-    Transaction: LegacyTransaction,
-} = require("@ethereumjs/tx");
 const rlp = require("rlp");
 const ethers = require("ethers");
-const { keccak256 } = require("ethers/lib/utils");
 
 const trie = new Trie();
 const blockHash = smallBlock;
-let provider = new ethers.providers.JsonRpcProvider();
-let block;
 
 function changeSyntax (tx){
     tx = {...tx};
+    /*
+        in block returned a transaction has 2 properties 'input' and 'gas'
+        that are expected as 'data' and 'gasLimit' 
+        modifications for ethereumjs 
+    */
     tx.data = tx.input;
     delete tx.input;
     tx.gasLimit = tx.gas;
@@ -23,66 +20,127 @@ function changeSyntax (tx){
     return tx;
 }
 
-async function recreateTransactionTrie (block, trie){
-    await Promise.all(block.transactions.map((tx, i) => 
-            (async (tx, i) => {
-                tx = changeSyntax(tx);
-                let TransactionClass;
-                switch (tx.type){
-                    case '0x0': TransactionClass = LegacyTransaction; break;
-                    case '0x1': TransactionClass = EIP2930Transaction; break;
-                    case '0x2': TransactionClass = EIP1559Transaction; break;
-                }
-                const serializedTx = TransactionClass.fromTxData(tx).serialize();
-                /* 
-                    something strange ...
-                    the blockchain stores values 0x0 expecting them to be interpreted as 0x80 in rlp! (infura ?)
-                    
-                     ¯\_(ツ)_/¯
+function formatRecursively (value) {
+    /* 
+        if value is an object place values in an array. 
+        rlp works on byte strings and arrays of byte strings.
+        rlp is agnostic of structure it is up to the consumer to interpret
+        this is similar to how a transaction is transformed from an object to array 
+    */
+    const encodable = value instanceof Object? Object.values(value) : value;
+    /*
+        if item is an array (list) recursively format internal items
+    */
+    if(Array.isArray(encodable)){
+        return [...encodable.map(element=>formatRecursively(element))]
+    }else{
+        /* 
+            this is a bit of a hack explicitly for transforming the value 0x0 as expressed in blocks on the blockchain
+            internally this value is expected to be expressed as the rlp encoding 80
+        */
+        if(value==="0x0") value = "0x";
+    }
+    return value;
+}
 
-                     rlp.encode("0x0")  === Uint8Array(1) [ 0 ]
-                     rlp.encode(0)      === Uint8Array(1) [ 128 ]
-                */
-                console.log('\n\ntransaction:\n');
-                console.log(`key: rlp encoded transaction index: ${tx.transactionIndex}\n`);
-                console.log(Buffer.from(rlp.encode(i)));
-                console.log('\nvalue: rlp encoded transaction \n')
-                console.log(serializedTx);
-                console.log(`\n\nis keccak hash of serialised transaction equal to hash in block: ${tx.hash ==keccak256(serializedTx)}`);
-                return await trie.put( rlp.encode(i), serializedTx);
-            })(tx, i) 
-        )); 
+const rulesForTxType = {
+    /* 
+        indicates LEGACY transaction 
+    */
+    '0x0' : {
+        /*
+            we must follow the rlp signature of a LEGACY transaction
+            
+            Legacy	    rlp([nonce, gasPrice, gasLimit, to, value, data, init, vrs ]) 
+        */
+        rlpFields : (tx) => {
+            const { nonce, gasPrice, gasLimit, to, value, data, v, r, s } = tx;
+            return [ nonce, gasPrice, gasLimit, to, value, data, v, r, s];
+        },
+        /* 
+            we must take into consideration  EIP-2718 for LEGACY transaction 
+        */
+        txEnvelope: (buffer) => buffer
+    },
+    /* 
+        indicates EIP-2930: "Optional access lists" transaction 
+    */
+    '0x1' : {
+        /* 
+            we must follow the rlp signature of a EIP-2930: "Optional access lists" transaction
+
+            Type 1	    0x01 || rlp([ chainId,  nonce,  gasPrice,  gasLimit,  to,  value,  data,  accessList,  signatureYParity,  signatureR,  signatureS ])     
+        */
+        rlpFields : (tx) => {
+            const { chainId, nonce, gasPrice, gasLimit, to, value, data, accessList, v, r, s } = tx;
+            return [ chainId, nonce, gasPrice, gasLimit, to, value, data, accessList, v, r, s ];
+        },
+        /* 
+            we must take into consideration EIP-2718 for EIP-2930: "Optional access lists" transaction
+
+            Type 2	    0x02 || rlp([ chain_id,  nonce,  max_priority_fee_per_gas,  max_fee_per_gas,  gas_limit,  destination,  amount,  data,  access_list,  signature_y_parity,  signature_r,  signature_s ])     
+        */
+        txEnvelope: (buffer) => {
+            const type = Buffer.from('01','hex');
+            return Buffer.concat([type, buffer])
+        }
+    },
+    /* 
+        indicates EIP-1559 transaction 
+    */
+    '0x2' : {
+        /* 
+            we must follow the rlp signature of a EIP-1559 transaction 
+        */
+        rlpFields : (tx) => {
+            const { chainId, nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, to, value, data, accessList, v, r, s } = tx;
+            return [ chainId, nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, to, value, data, accessList, v, r, s ];
+        },
+        /* 
+            we must take into consideration  EIP-2718 for EIP-1559 transaction 
+        */
+        txEnvelope: (buffer) => {
+            const type = Buffer.from('02','hex');
+            return Buffer.concat([type, buffer]);
+        }
+    }   
+
 };
 
-(async (blockHash)=> {
-    block = await provider.send("eth_getBlockByHash", [ blockHash, true ]);
-    await recreateTransactionTrie(block, trie);
-    console.log('\n\nStructure of our Trie\n')
-    await inspectTrie(trie);
+(async()=>{
+    
+    const provider = new ethers.providers.JsonRpcProvider();
+    const block = await provider.send("eth_getBlockByHash",[ blockHash, true ]);
 
-})(blockHash);
-
-
-async function inspectTrie(trie){
-    await trie.walkTrie(trie.root, 
-        async (nodeRef, node, keyProgress, walkController) => {
+    await Promise.all(block.transactions.map((tx, i) => 
+        (async (tx, i) => {
+            /* 
+                account for naming conventions 
+            */
+            tx = changeSyntax(tx);
+            const { rlpFields, txEnvelope } = rulesForTxType[tx.type];
+            /* 
+                follow rlp siganture for specific tx type 
+            */
+            const rlpData = rlpFields(tx);
+            /* 
+                format data to make it rlp encodable 
+            */
+            const rlpEncodable = formatRecursively(rlpData);
+            /* 
+                complete serialisation (EIP-2718) 
+            */
+            const rlpBytes = Buffer.from(rlp.encode(rlpEncodable));
+            const completeSerialization = txEnvelope(rlpBytes);
             
-            walkController.allChildren(node)
+            return await trie.put( rlp.encode(i), completeSerialization );
+        })(tx, i) 
+    ));
 
-            if (node instanceof LeafNode) {
-                console.log("LeafNode");
-                return 
-            }
+    console.log(`block.transactionsRoot matches trie.root : ${block.transactionsRoot === ethers.utils.hexlify(trie.root)}`);
+    
+})();
 
-            if (node instanceof ExtensionNode) {
-                console.log("ExtensionNode");
-                return 
-            }
-            
-            if (node instanceof BranchNode) {
-                console.log("BranchNode");
-                return 
-            } 
 
-        });
-}
+
+
