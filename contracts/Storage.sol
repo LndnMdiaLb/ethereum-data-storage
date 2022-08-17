85e882fb52ff480cba618cef729747acf9e3ba41
Andreas Dilaveris <projects@thelondonmedialab.com>
2022-08-17 10:39:10 +0300

    Basic data packing

--- contracts/Storage.sol
+++ contracts/Storage.sol
@@ -2,5 +2,12 @@
 pragma solidity >0.5.0 <=0.8.0;
 
 contract Storage {
+    /* 
+        uint 256 is a 32 byte value 
+    */
     uint8 slotUint8_1 = 1;
+    uint16 slotUint16_1 = 1;
+    // uint256 slotUint256_1 = 1;
+    // uint128 slotUint128_1 = 1;
+    uint32 slotUint32_1 = 1;
 }
